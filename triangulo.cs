02ef7166715777de3f50e6bef84f4867c54090e9
‘MiguelColl’ <ayorino23@hotmail.com>
2022-10-03 16:31:19 +0200

    'Actualizacion'

--- triangulo.cs
+++ triangulo.cs
@@ -4,30 +4,33 @@ using System;
 
 class Triangulo{
 	static void Main(){
-		try{
-			int asterisco=1, altura;
-			
-			do{
-				Console.Write("Introduce una altura: ");
+		int asterisco=1, altura=1;
+		bool error;
+										
+		do{		
+			error=false;	
+			try{
+				Console.Write("Introduce una altura(minimo 2): ");
 				altura=Convert.ToInt32(Console.ReadLine());
 				if(altura<=1)
 					Console.WriteLine("Error: altura minima 2");
-			}while(altura<=1);
-			
-			for(int contador=1;contador<altura;contador++)
-				asterisco+=2;
-			
-			for(int piso=1;piso<=altura;piso++){
-				for(int espacios=piso;espacios>1;espacios--)
-					Console.Write(" ");
-				for(int ancho=1;ancho<=asterisco;ancho++)
-					Console.Write("*");
-				Console.WriteLine();
-				asterisco-=2;
 			}
-		}
-		catch(Exception error){
-			Console.WriteLine("Error: "+error.Message);
+			catch(Exception errorEncontrado){
+				Console.WriteLine("Error: "+errorEncontrado.Message);
+				error=true;
+			}
+		}while((altura<=1)&&(error=true));
+					
+		for(int contador=1;contador<altura;contador++)
+			asterisco+=2;
+			
+		for(int piso=1;piso<=altura;piso++){
+			for(int espacios=piso;espacios>1;espacios--)
+				Console.Write(" ");
+			for(int ancho=1;ancho<=asterisco;ancho++)
+				Console.Write("*");
+			Console.WriteLine();
+			asterisco-=2;
 		}
 	}
 }
